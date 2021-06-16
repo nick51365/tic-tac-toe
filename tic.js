fd73ebc97947c612ff0f4805f46ed411b8666f9f
nick <nick51365@gmail.com>
2021-06-16 19:56:43 -0400

    added functionality to check if a tie game has occured

--- tic.js
+++ tic.js
@@ -1,8 +1,6 @@
-
 //gameBoard module
 let gameBoard = (function (){
     
-    //Returns all PUBLIC variables and functions
     return {
         boardSpaces : ["","","","","","","","",""]
     };
@@ -14,10 +12,23 @@ let displayController = (function(){
     //Variable to hold name of player who's turn it currently is
     let whosTurn = "playerOne";
 
+    //Total number of moves made. For use determining if a tie has occured
+    let movesMade = 0;
+
     //Initialize "turnDisplay" at beginning of game
     let turnDisplay = document.getElementById("turnDisplay");
     turnDisplay.innerHTML = "Player One's Turn";
 
+    //Reset the game
+    let resetGame = function(){
+        gameBoard.boardSpaces = ["","","","","","","","",""];
+        displayController.displayMarks();
+        turnDisplay.innerHTML = "Player One's Turn"
+        whosTurn = "playerOne";
+        movesMade = 0;
+    };
+
+    //Check if a winning combination of spaces has been filled
     let checkWin = function(){
         let winner = "";   
         let winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
@@ -38,29 +49,26 @@ let displayController = (function(){
                 }else{
                     alert("Player Two Wins!");
                 }       
-                gameBoard.boardSpaces = ["","","","","","","","",""];
-                displayController.displayMarks();
-                turnDisplay.innerHTML = "Player One's Turn"
-                whosTurn = "playerOne";
+                resetGame();
             };
         },50); 
     };
-    //Checks if the game has reached a tie with no more available spaces
+    //Checks if the game has reached a tie 
     //If so, announces a tie and resets the board
     let checkTie = function(){
-        for (i = 0; i < gameBoard.boardSpaces.length; i++){
-            if (gameBoard.boardSpaces[i] === ""){
-                console.log(gameBoard.boardSpaces[i]);
-                break;
-            };
-            console.log("TIE");
-        };
+        if (movesMade === 9){
+            setTimeout(function(){
+                alert("Tie!");
+                resetGame();
+            },50);
+        }
     };
 
     return{
         markSpace : function(i){
             if(gameBoard.boardSpaces[i] === ""){
                 if (whosTurn === "playerOne"){
+                    movesMade++;
                     gameBoard.boardSpaces[i] = "X";
                     this.displayMarks();
                     checkWin();
@@ -68,6 +76,7 @@ let displayController = (function(){
                     whosTurn = "playerTwo";
                     turnDisplay.innerHTML = "Player Two's Turn";
                 }else if (whosTurn === "playerTwo"){
+                    movesMade++;
                     gameBoard.boardSpaces[i] = "O";
                     this.displayMarks();
                     checkWin();
@@ -104,7 +113,7 @@ for (i = 0; i < spaces.length; i++){
     })
 };
 
-displayController.displayMarks();
+//Create two player objects
 const playerOne = playerFactory("X");
 const playerTwo = playerFactory("O");
 
