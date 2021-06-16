6be9e22cb7634d4fc70d1bccd843f7e2247752fe
nick <nick51365@gmail.com>
2021-06-15 21:34:24 -0400

    styling changes/bug fixes

--- tic.js
+++ tic.js
@@ -23,24 +23,38 @@ let displayController = (function(){
         let winningCombos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
         //Iterate through winningCombos to check if any are completed
         for (i = 0;i < winningCombos.length; i++){
-            console.log(winningCombos[i]);
             if (gameBoard.boardSpaces[winningCombos[i][0]] != ""){
                 if (gameBoard.boardSpaces[winningCombos[i][0]] === gameBoard.boardSpaces[winningCombos[i][1]] &&
                     gameBoard.boardSpaces[winningCombos[i][1]] === gameBoard.boardSpaces[winningCombos[i][2]]){
                     winner = whosTurn;
                 };
-        }
-    };
-
+            }
+        };
         //If a winner is declared, announce winner and clear board
         setTimeout(function(){
             if (winner != ""){
-                alert(winner + " wins!");
+                if (winner === "playerOne"){
+                    alert("Player One Wins!");
+                }else{
+                    alert("Player Two Wins!");
+                }       
                 gameBoard.boardSpaces = ["","","","","","","","",""];
                 displayController.displayMarks();
+                turnDisplay.innerHTML = "Player One's Turn"
                 whosTurn = "playerOne";
             };
         },50); 
+    };
+    //Checks if the game has reached a tie with no more available spaces
+    //If so, announces a tie and resets the board
+    let checkTie = function(){
+        for (i = 0; i < gameBoard.boardSpaces.length; i++){
+            if (gameBoard.boardSpaces[i] === ""){
+                console.log(gameBoard.boardSpaces[i]);
+                break;
+            };
+            console.log("TIE");
+        };
     };
 
     return{
@@ -50,12 +64,14 @@ let displayController = (function(){
                     gameBoard.boardSpaces[i] = "X";
                     this.displayMarks();
                     checkWin();
+                    checkTie();
                     whosTurn = "playerTwo";
                     turnDisplay.innerHTML = "Player Two's Turn";
                 }else if (whosTurn === "playerTwo"){
                     gameBoard.boardSpaces[i] = "O";
                     this.displayMarks();
                     checkWin();
+                    checkTie();
                     whosTurn = "playerOne";
                     turnDisplay.innerHTML = "Player One's Turn";
                 }
